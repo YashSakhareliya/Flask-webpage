cf8c4e6cf4d956cfdd7c8cb57d0501af5608ab66
YashSakhareliya <160346294+YashSakhareliya@users.noreply.github.com>
2024-03-10 18:14:32 +0000

    added dynamic data and some api route

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
+Project = [
+  {
+    'id': 1,
+    'title': 'Django Project',
+    'platform':'vs code',
+    'description':'This is a project for learning django'  
+  },
+  {
+    'id': 2,
+    'title': 'Flask Project',
+    'platform':'pycham',
+    'description':'This is a project for learning Flask'  
+  },
+  {
+    'id': 3,
+    'title': 'Login pages',
+    'platform':'vs code',
+    'description':'This is login page for upload on your website'  
+  },
+  {
+    'id': 4,
+    'title': 'Templates',
+    'description':'This is a project for use templates'  
+  }
+]
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',projects=Project)
+
+@app.route('/api/project')
+def project():
+  return jsonify(Project)
+
 
 
 if __name__ == '__main__':
